3635929f25c22e4e00a4fbd8c1c7e152198022e6
Yathin Babu N <anythingicode@gmail.com>
2021-04-13 18:14:43 +0530

    home page added

---
+++ script.js
@@ -0,0 +1,12 @@
+// Nav bar section starts
+
+function myFunction() {
+    var x = document.getElementById("nav-bar");
+    if (x.className === "nav-bar") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav-bar";
+    }
+  }
+
+//Nav bar sections ends
